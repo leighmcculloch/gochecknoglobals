0acea9f9f0ff72a7f40ae35a014a8ed948642825
Simon Sawert <simon@sawert.se>
2020-10-07 22:59:51 +0200

    Default to package ./...

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main // import "4d63.com/gochecknoglobals"
 
 import (
+	"os"
+
 	"4d63.com/gochecknoglobals/checknoglobals"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
 func main() {
+	if len(os.Args) == 1 {
+		os.Args = append(os.Args, "./...")
+	}
+
 	singlechecker.Main(checknoglobals.Analyzer())
 }
